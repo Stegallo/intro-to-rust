a6c51564f4369baa6d3fbf39a010e8cb3701f722
Stegallo <stegallo@gmail.com>
2023-11-01 15:54:06 -0700

    variables

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,5 +1,14 @@
-use std::io;
+// use std::io;
 
 fn main() {
+    // Variables
+    let x = 0; // this is immutable
     println!("Hello World!");
+    println!("{}", x);
+    println!("{x}");
+
+    let mut y = 1;
+    println!("{y}");
+    y = 2;
+    println!("{y}")
 }
